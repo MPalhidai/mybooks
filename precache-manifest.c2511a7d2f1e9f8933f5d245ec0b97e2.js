0aedcf82567217a6a97eafb8acacce326c93d1f8
Michael Palhidai <mpalhidai@gmail.com>
2018-11-10 00:49:19 -0800

    Updates

---
+++ precache-manifest.c2511a7d2f1e9f8933f5d245ec0b97e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "960cc1035218773bcd35",
+    "url": "/bookstore/static/css/main.4f894a8a.chunk.css"
+  },
+  {
+    "revision": "960cc1035218773bcd35",
+    "url": "/bookstore/static/js/main.960cc103.chunk.js"
+  },
+  {
+    "revision": "f56344e499b43b2851f5",
+    "url": "/bookstore/static/js/1.f56344e4.chunk.js"
+  },
+  {
+    "revision": "78b710cb0798a0e1838f",
+    "url": "/bookstore/static/js/runtime~main.78b710cb.js"
+  },
+  {
+    "revision": "54527a094851bb44213de207b35c1a10",
+    "url": "/bookstore/index.html"
+  }
+];
