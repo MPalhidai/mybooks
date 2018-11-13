1fd6fd72c9bf70f026e08af08bc99761a11a3240
Michael Palhidai <mpalhidai@gmail.com>
2018-11-13 13:26:22 -0800

    style v2

--- src/components/Book.js
+++ src/components/Book.js
@@ -5,16 +5,34 @@ export class Book extends React.Component {
   render() {
     return (
       <div className = 'bookCard' key = { this.props.key } id = { this.props.id }>
-        <p className = 'bookCategory'>{ this.props.category }</p>
-        <p className = 'bookTitle'>{ this.props.title }</p>
-        <p className = 'bookAuthor'>Author</p>
-        <p className = 'bookLinks'>
-          <span className = 'bookLink'>Comments</span>
-          <span className = 'bookLink' onClick = { () => this.props.removeBtn(this.props.id) }>
-            Remove
-          </span>
-          <span className = 'bookLink'>Edit</span>
-        </p>
+        <div className = 'bookCardLeft'>
+          <p className = 'bookCategory'>{ this.props.category }</p>
+          <p className = 'bookTitle'>{ this.props.title }</p>
+          <p className = 'bookAuthor'>Author</p>
+          <p className = 'bookLinks'>
+            <span className = 'bookLink'>Comments</span>
+            <span className = 'bookLink' onClick = { () => this.props.removeBtn(this.props.id) }>
+              Remove
+            </span>
+            <span className = 'bookLink'>Edit</span>
+          </p>
+        </div>
+        <div className = 'bookCardMiddle'>
+          <div className = 'bookProgressBar'>
+            
+          </div>
+          <div className = 'bookCardMiddleRight'>
+            <p className = 'bookProgressNumber'>0%</p>
+            <p className = 'bookProgressComplete'>Completed</p>
+          </div>
+        </div>
+        <div className = 'bookCardRight'>
+          <p className = 'bookChapterHeader'>CURRENT CHAPTER</p>
+          <p className = 'bookChapter'>Introduction</p>
+          <button className = 'progressBtn' >
+            Update Progress
+          </button>
+        </div>
       </div>
     );
   }
