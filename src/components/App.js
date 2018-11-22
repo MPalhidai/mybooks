e12dbd179ff163099975c93fe580e349c54244ae
Michael Palhidai <mpalhidai@gmail.com>
2018-11-21 19:27:42 -0800

    body switching ready for new components

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import '../css/App.css';
-
-import BookForm from '../containers/BookForm';
 import { Nav } from './Nav';
 import { Routes } from './Routes';
 
@@ -12,7 +10,6 @@ export class App extends React.Component {
         <Nav />
         <div className = 'container'>
           <Routes />
-          <BookForm />
         </div>
       </div>
     );
--- src/components/Routes.js
+++ src/components/Routes.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import BookList from '../containers/BookList';
+import BookForm from '../containers/BookForm';
 
 export class Routes extends React.Component {
   render() {
     return(
       <Switch>
-        <Route exact path = '/' component = { BookList } />
+        <Route exact path = '/' render = { props =>
+          <div>
+            <BookList />
+            <BookForm />
+          </div>
+        } />
+
+
       </Switch>
     );
   }
