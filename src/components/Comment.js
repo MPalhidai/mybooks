239cca9a132b7a7b5c54bd3e1f0b8011947ed7e1
Michael Palhidai <mpalhidai@gmail.com>
2018-11-23 22:14:27 -0800

    comment style

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import '../css/Comment.css';
 
-// Comment - rating, content, user_id, book_id
+// comment - rating, content, user_id, book_id
 // Cool feature if the comment could check how much the user has read the book
 
 export class Comment extends React.Component {
   render() {
     return (
-      <div className = 'CommentCard' id = { this.props.id }>
-        <div className = 'CommentCardLeft'>
-          <p className = 'CommentUser'>{ this.props.user }</p>
+      <div className = 'commentCard' id = { this.props.id }>
+        <div className = 'flexDiv'>
+          <div className = 'commentCardLeft'>
+            <p className = 'commentUser'>User: { this.props.user }</p>
+          </div>
+          <div className = 'commentCardRight'>
+            <p className = 'commentRating'>Rating: { this.props.rating }</p>
+          </div>
         </div>
-        <div className = 'CommentCardRight'>
-          <p className = 'CommentRating'>{ this.props.rating }</p>
+        <div className = 'commentCardBottom'>
+          <p className = 'commentContent'>{ this.props.content }</p>
         </div>
-        <div className = 'CommentCardBottom'>
-          <p className = 'CommentContent'>{ this.props.content }</p>
-        </div>
+
       </div>
     );
   }
